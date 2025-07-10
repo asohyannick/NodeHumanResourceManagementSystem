2472810cb7cabcc69bebcbd176096b74c8e14690
asohyannick <asohyannick3@gmail.com>
2025-07-09 17:42:18 -0700

    Developers, coders and programmers worldwide can now read and know more about this project by reading the comprehensive README.FILE which holds all the detail information about our project

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const port = 8080;
 //test API endpoint
 app.get('/api/v1/test', (_req, res) => {
     return res.status(200).send('Hello world');
-})
+});
 app.listen(port, () => {
     console.log(`Server is running on port ${port}...`);
 })
