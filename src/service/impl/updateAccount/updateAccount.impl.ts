4de35a9166be447d3ac031428f905b9af3004122
asohyannick <asohyannick3@gmail.com>
2025-07-10 16:55:29 -0700

    Admin can now edit and update a single available  user who is  registered in our system successfully!

---
+++ src/service/impl/updateAccount/updateAccount.impl.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import Auth from "../../../model/auth/auth.model";
+const updateAccount = async (req: Request, res: Response): Promise<Response> => {
+    try {
+        const { id } = req.params;
+        const {
+            firstName,
+            lastName,
+            email,
+            password,
+        } = req.body;
+        const user = await Auth.findByIdAndUpdate(id, {
+            firstName,
+            lastName,
+            email,
+            password,
+            isAdmin: true,
+        }, { new: true, runValidators: true });
+        if (!user) {
+            return res.status(StatusCodes.NOT_FOUND).json({ message: "User's account doesn't exist!" });
+        }
+        return res.status(StatusCodes.OK).json({
+            success: true,
+            message: "User account has been updated successfully!",
+            user,
+        });
+    } catch (error) {
+        console.error("Error occured!", error);
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            success: false,
+            message: "Something went wrong!",
+            error: error instanceof Error ? error.message : "Unknown Error Message",
+        });
+    }
+}
+
+export default updateAccount;
