542cd837538fc3e64e38b87876ad3e231f608c9d
asohyannick <asohyannick3@gmail.com>
2025-07-19 05:06:52 -0700

    Admin should be able to removed permanently or delete a single available employee job opportunity saved in our system successfully!

---
+++ src/service/impl/job/deleteJob/deleteJob.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import Job from "../../../../model/job/job.model";
+const deleteJob = async (req: Request, res: Response): Promise<Response> => {
+    try {
+        const { id } = req.params;
+        const showJob = await Job.findByIdAndDelete(id);
+        if (!showJob) {
+            return res.status(StatusCodes.NOT_FOUND).json({
+                success: false,
+                message: "Job doesn't exist!"
+            });
+        }
+        return res.status(StatusCodes.OK).json({
+            success: true,
+            message: "A single available employee job has been deleted successfully!",
+            showJob
+        });
+    } catch (error) {
+        console.error("Error occured!", error);
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            success: false,
+            message: "Something went wrong!",
+            error: error instanceof Error ? error.message : "Unknown  Error Message",
+        });
+    }
+}
+export default deleteJob;
