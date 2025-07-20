3fc39f9205eed3f74070f0af0c3c4076b0c920db
asohyannick <asohyannick3@gmail.com>
2025-07-20 06:38:24 -0700

    Employee should be able to fetch and view a single available document uploaded for job application and processing in our application successfully!

---
+++ src/service/impl/document/showDocument/showDocument.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import DocumentManagementModel from "../../../../model/document/document.model";
+const showUploadedDocument = async (req: Request, res: Response): Promise<Response> => {
+    try {
+        const { id } = req.params;
+        const document = await DocumentManagementModel.findById(id);
+        if(!document) {
+            return res.status(StatusCodes.NOT_FOUND).json({
+                success: false,
+                message: "Employee document doesn't exist!"
+            });
+        }
+        return res.status(StatusCodes.OK).json({
+            success: true,
+            message: "A single available employee uploaded document has been fetched successfully from the backend!",
+            document,
+        });
+    } catch (error) {
+        console.error("Error occured!", error);
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            success: false,
+            message: "Something went wrong!",
+            error: error instanceof Error ? error.message : "Unknown Error Message",
+        });
+    }
+}
+
+export default showUploadedDocument;
